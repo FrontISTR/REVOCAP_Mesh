7fe8d28223183a8eb693d08db75abc180470d41e
TOKUNAGA Ken-ichi <tokunaga@advancesoft.jp>
2018-01-18 14:29:00 +0900

    STLのASCII判定方法の修正

--- RevocapIO/kmbStereolithographyIO.cpp
+++ RevocapIO/kmbStereolithographyIO.cpp
@@ -1,6 +1,7 @@
 #include "RevocapIO/kmbStereolithographyIO.h"
 
 #include <fstream>
+#include <iostream>
 
 kmb::StereolithographyIO::fileType kmb::StereolithographyIO::checkFormat(std::string filename)
 {
@@ -8,12 +9,14 @@ kmb::StereolithographyIO::fileType kmb::StereolithographyIO::checkFormat(std::st
 	input.seekg(0,std::ios::beg);
 	char buf[80];
  	input.read(buf,80);
- 	for(int i=0;i<80;++i){
- 		if(buf[i] == '\n' || buf[i] == '\r'){
- 			input.close();
- 			return kmb::StereolithographyIO::kASCII;
- 		}
- 	}
+	if (strncmp(buf, "solid", 5)==0) {
+		for (int i = 0; i < 80; ++i) {
+			if (buf[i] == '\n' || buf[i] == '\r') {
+				input.close();
+				return kmb::StereolithographyIO::kASCII;
+			}
+		}
+	}
  	input.close();
 	return kmb::StereolithographyIO::kBINARY;
 }
