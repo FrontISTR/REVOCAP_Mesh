91dbabc92a612f960a9b95d7ffde79c1c9b651c0
TOKUNAGA Ken-ichi <tokunaga@advancesoft.jp>
2022-09-19 15:03:20 +0900

    add io test

--- Test/RevocapIO_Test.cpp
+++ Test/RevocapIO_Test.cpp
@@ -7,16 +7,48 @@
 #include <boost/test/unit_test.hpp>
 #endif
 
-#include <fstream>
+#include "Test/Test_Common.h"
 #include "MeshDB/kmbMeshData.h"
 #include "RevocapIO/kmbMicroAVSIO.h"
+#include "RevocapIO/kmbHecmwIO.h"
+#include "RevocapIO/kmbFrontISTRIO.h"
+#include "RevocapIO/kmbRevocapIOUtils.h"
 
 BOOST_AUTO_TEST_SUITE(RevocapIO)
 
-BOOST_AUTO_TEST_CASE(STLIO)
+BOOST_AUTO_TEST_CASE(FrontISTRIO)
 {
-	int n=0;
-	BOOST_CHECK_EQUAL(n,0);
+	kmb::MeshData* mesh = new kmb::MeshData();
+	mesh->beginNode();
+	mesh->addNodeWithId(0.0, 0.0, 0.0, 0);
+	mesh->addNodeWithId(1.0, 0.0, 0.0, 1);
+	mesh->addNodeWithId(0.0, 1.0, 0.0, 10);
+	mesh->addNodeWithId(0.0, 0.0, 1.0, 100);
+	mesh->endNode();
+	kmb::nodeIdType nodes[4] = { 0,1,10,100 };
+	mesh->beginElement();
+	mesh->addElementWithId(kmb::kTetrahedron, nodes, 10);
+	mesh->endElement();
+	kmb::DataBindings* data = mesh->createDataBindings("BND0", kmb::DataBindings::kNodeGroup, kmb::PhysicalValue::None, "BOUNDARY");
+	data->addId(0);
+	kmb::HecmwIO hecmw;
+	hecmw.saveToMeshFile_3_6("FrontISTRTest_hecIO.msh", mesh);
+	hecmw.appendFooterToFile("FrontISTRTest_hecIO.msh");
+	mesh->clearModel();
+	delete mesh;
+
+	mesh = new kmb::MeshData();
+	kmb::FrontISTRIO fistr;
+	mesh->createDataBindings("BND0", kmb::DataBindings::kNodeGroup, kmb::PhysicalValue::Vector3, "BOUNDARY");
+	fistr.loadMesh("FrontISTRTest_hecIO.msh", mesh);
+	fistr.saveMesh("FrontISTRTest_meshdata.msh", mesh);
+	BOOST_CHECK_EQUAL(mesh->getNodeCount(),4);
+	BOOST_CHECK_EQUAL(mesh->getElementCount(), 1);
+	BOOST_CHECK(mesh->hasData("BND0"));
+	BOOST_CHECK_EQUAL(mesh->getIdCount("BND0"), 1);
+	BOOST_CHECK_EQUAL(mesh->getData("BND0")->getBindingMode(), kmb::DataBindings::kNodeGroup);
+	BOOST_CHECK(mesh->getData("BND0")->hasId(0));
+	delete mesh;
 }
 
 BOOST_AUTO_TEST_CASE(MicroAVSIO_MeshData)
@@ -43,6 +75,13 @@ BOOST_AUTO_TEST_CASE(MicroAVSIO_MeshData)
 	BOOST_CHECK_EQUAL(mesh1->getElementCount(),1);
 	delete mesh0;
 	delete mesh1;
+}
+
+BOOST_AUTO_TEST_CASE(RevocapIOUtilsTest)
+{
+	std::string line = "!SGROUP, SGRP = master";
+	std::string val = kmb::RevocapIOUtils::getValue(line, "SGRP");
+	BOOST_CHECK_EQUAL(val, "master");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
